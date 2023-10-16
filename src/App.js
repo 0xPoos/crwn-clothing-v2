dee089222ceaae1ea8d4559a0c15f000eb4d70ab
0xPoos <0xPoos@gmail.com>
2023-10-15 19:50:12 -0500

    iteratate over categories

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Men',
+    },
+    {
+      id: 5,
+      title: 'Women',
+    },
+  ];
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img/> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Show Now</p>
+      {categories.map(({ id, title }) => (
+        <div className='category-container' key={id}>
+          <div className="background-image"><div/>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Show Now</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img/> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Show Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img/> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Show Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img/> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img/> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
