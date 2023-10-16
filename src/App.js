d7ad43604cc36a647c0fe695b169447446bd7c67
0xPoos <0xPoos@gmail.com>
2023-10-15 19:35:32 -0500

    content starter

--- src/App.js
+++ src/App.js
@@ -2,38 +2,36 @@ const App = () => {
   return (
     <div className='categories-container'>
       <div className='category-container'>
-        {/* <img /> */}
+        {/* <img/> */}
         <div className='category-body-container'>
           <h2>Hats</h2>
-          <p>Shop Now</p>
+          <p>Show Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        {/* <img/> */}
         <div className='category-body-container'>
           <h2>Jackets</h2>
-          <p>Shop Now</p>
+          <p>Show Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        {/* <img/> */}
         <div className='category-body-container'>
           <h2>Sneakers</h2>
-          <p>Shop Now</p>
+          <p>Show Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        {/* <img/> */}
         <div className='category-body-container'>
           <h2>Womens</h2>
-          <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        {/* <img/> */}
         <div className='category-body-container'>
           <h2>Mens</h2>
-          <p>Shop Now</p>
         </div>
       </div>
     </div>
